2e3dbed3dd78c33bc6f2bf049a3f7e1e7287c079
Devonaut <30209881@blackpool.ac.uk>
2023-09-24 18:12:31 +0100

    Wall Running Created

--- fps-parkour/Assets/Scripts/WallRunning.cs
+++ fps-parkour/Assets/Scripts/WallRunning.cs
@@ -4,77 +4,121 @@ using UnityEngine;
 
 public class WallRunning : MonoBehaviour
 {
-  [Header("WallRunning")]
-  public LayerMask whatIsWall;
-  public LayerMask whatIsGround;
-  public float wallRunForce;
-  public float  maxWallRunTime;
-  private float wallRunTimer;
+    [Header("Wallrunning")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float wallClimbSpeed;
+    public float maxWallRunTime;
+    private float wallRunTimer;
 
-  [Header("Input")]
-  private float horizontalInput;
-  private float verticalInput;
+    [Header("Input")]
+    public KeyCode upwardsRunKey = KeyCode.LeftShift;
+    public KeyCode downwardsRunKey = KeyCode.LeftControl;
+    private bool upwardsRunning;
+    private bool downwardsRunning;
+    private float horizontalInput;
+    private float verticalInput;
 
-  [Header("Detection")]
-  public float wallCheckDistance;
-  public float minJumpHeight;
-  private RaycastHit leftWallhit;
-  private RaycastHit rightWallhit;
-  private bool wallLeft;
-  private bool wallRight;
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallhit;
+    private RaycastHit rightWallhit;
+    private bool wallLeft;
+    private bool wallRight;
 
-  [Header("Reference")]
-  public Transform orientation;
-  private PlayerMovementAdvanced pm;
-  private Rigidbody rb;
+    [Header("References")]
+    public Transform orientation;
+    private PlayerMovementAdvanced pm;
+    private Rigidbody rb;
 
-  private void Start()
-  {
-    rb = GetComponent<Rigidbody>();
-    pm = GetComponent<PlayerMovementAdvanced>();
-  }
-
-  private void Update()
-  {
-    CheckForWall();
-  }
-
-  private void CheckForWall()
-  {
-    wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
-    wallLeft = Physics.Raycast(transform.position, -orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
-  }
-
-  private bool AboveGround()
-  {
-    return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
-  }
-
-  private void StateMachine()
-  {
-    // Getting Inputs
-    horizontalInput = Input.GetAxisRaw("Horizontal");
-    verticalInput = Input.GetAxisRaw("Vertical");
-
-    // State 1 - Wallrunning
-    if((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+    private void Start()
     {
-        // start wallrun here
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovementAdvanced>();
     }
-  }
 
-  private void StartWallRun()
-  {
+    private void Update()
+    {
+        CheckForWall();
+        StateMachine();
+    }
 
-  }
+    private void FixedUpdate()
+    {
+        if (pm.wallrunning)
+            WallRunningMovement();
+    }
 
-  private void WallRunningMovement()
-  {
+    private void CheckForWall()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallhit, wallCheckDistance, whatIsWall);
+    }
 
-  }
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+    }
 
-  private void StopWallRun()
-  {
-    
-  }
+    private void StateMachine()
+    {
+        // Getting Inputs
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        upwardsRunning = Input.GetKey(upwardsRunKey);
+        downwardsRunning = Input.GetKey(downwardsRunKey);
+
+        // State 1 - Wallrunning
+        if((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+        {
+            if (!pm.wallrunning)
+                StartWallRun();
+        }
+
+        // State 3 - None
+        else
+        {
+            if (pm.wallrunning)
+                StopWallRun();
+        }
+    }
+
+    private void StartWallRun()
+    {
+        pm.wallrunning = true;
+    }
+
+    private void WallRunningMovement()
+    {
+        rb.useGravity = false;
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        Vector3 wallNormal = wallRight ? rightWallhit.normal : leftWallhit.normal;
+
+        Vector3 wallForward = Vector3.Cross(wallNormal, transform.up);
+
+        if ((orientation.forward - wallForward).magnitude > (orientation.forward - -wallForward).magnitude)
+            wallForward = -wallForward;
+
+        // forward force
+        rb.AddForce(wallForward * wallRunForce, ForceMode.Force);
+
+        // upwards/downwards force
+        if (upwardsRunning)
+            rb.velocity = new Vector3(rb.velocity.x, wallClimbSpeed, rb.velocity.z);
+        if (downwardsRunning)
+            rb.velocity = new Vector3(rb.velocity.x, -wallClimbSpeed, rb.velocity.z);
+
+        // push to wall force
+        if (!(wallLeft && horizontalInput > 0) && !(wallRight && horizontalInput < 0))
+            rb.AddForce(-wallNormal * 100, ForceMode.Force);
+    }
+
+    private void StopWallRun()
+    {
+        pm.wallrunning = false;
+    }
 }
