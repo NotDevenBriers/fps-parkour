0ac3b2c92aedc76342f94a61bebd2f02502b4877
TheRealNerdy <benmusto29@gmail.com>
2023-10-03 17:44:35 +0100

    Inventory and grapple
    -Added holster transforms
    -1-4 allows selecting different weapon types you have in your inventory
    -Pick up and drop anything that has a box collider and the weapon script and being tagged weapons.
    -The grapple gun script is now disabled if the player doesnt not have a grapple in their hands.
    -So just a stable-ish version of how the inventory is going to work.
    
    TO NOTE! thinking about multiplayer and how the grapple will interact between a player who just dropped it and the new player picking it up.. i feel like there will be issues but i am not sure.

--- fps-parkour/Assets/Scripts/Inventory.cs
+++ fps-parkour/Assets/Scripts/Inventory.cs
@@ -1,14 +1,39 @@
+using System;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public GameObject HandSlot; // The current weapon in hand
+    // Game Objects
+    public GameObject HandSlot;
     public GameObject Primary;
     public GameObject Secondary;
     public GameObject Grenade;
-    public GameObject Grapple; // Added Grapple variable
+    public GameObject Grapple;
+    public GameObject Player;
 
-    // This is called when a weapon is picked up
+    // Transform positions
+    public Transform weaponHoldPosition;
+    public Transform PrimaryHolsterPosition;
+    public Transform SecondaryHolsterPosition;
+    public Transform GrenadeHolsterPosition;
+    public Transform GrappleHolsterPosition;
+
+    // Other variables
+    public Pickup pickUpScript;
+    public bool grappleable;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && Primary != null) EquipWeapon(Primary);
+        if (Input.GetKeyDown(KeyCode.Alpha2) && Secondary != null) EquipWeapon(Secondary);
+        if (Input.GetKeyDown(KeyCode.Alpha3) && Grenade != null) EquipWeapon(Grenade);
+        if (Input.GetKeyDown(KeyCode.Alpha4) && Grapple != null)
+        {
+            EquipWeapon(Grapple);
+            grappleable = true;
+        }
+    }
+
     public bool AddWeapon(GameObject weapon)
     {
         Weapon weaponScript = weapon.GetComponent<Weapon>();
@@ -22,7 +47,8 @@ public class Inventory : MonoBehaviour
                     EquipWeapon(weapon);
                     return true;
                 }
-                break;
+                HolsterWeapon(weapon, PrimaryHolsterPosition);
+                return true;
 
             case Weapon.WeaponType.Secondary:
                 if (Secondary == null)
@@ -31,7 +57,8 @@ public class Inventory : MonoBehaviour
                     EquipWeapon(weapon);
                     return true;
                 }
-                break;
+                HolsterWeapon(weapon, SecondaryHolsterPosition);
+                return true;
 
             case Weapon.WeaponType.Grenade:
                 if (Grenade == null)
@@ -40,16 +67,18 @@ public class Inventory : MonoBehaviour
                     EquipWeapon(weapon);
                     return true;
                 }
-                break;
+                HolsterWeapon(weapon, GrenadeHolsterPosition);
+                return true;
 
-            case Weapon.WeaponType.Grapple: // Added Grapple case
+            case Weapon.WeaponType.Grapple:
                 if (Grapple == null)
                 {
                     Grapple = weapon;
                     EquipWeapon(weapon);
                     return true;
                 }
-                break;
+                HolsterWeapon(weapon, GrappleHolsterPosition);
+                return true;
         }
 
         return false;
@@ -57,72 +86,87 @@ public class Inventory : MonoBehaviour
 
     private void EquipWeapon(GameObject weapon)
     {
-        if (HandSlot != null)
-        {
-            HandSlot.SetActive(false);
-        }
+        grappleable = false;
+        HolsterWeapon(Primary, PrimaryHolsterPosition);
+        HolsterWeapon(Secondary, SecondaryHolsterPosition);
+        HolsterWeapon(Grenade, GrenadeHolsterPosition);
+        HolsterWeapon(Grapple, GrappleHolsterPosition);
 
-        weapon.SetActive(true);
-        HandSlot = weapon;
+        if (weapon != null)
+        {
+            weapon.transform.SetParent(weaponHoldPosition);
+            weapon.SetActive(true);
+            weapon.transform.localPosition = Vector3.zero;
+            weapon.transform.localRotation = Quaternion.identity;
+
+            if (weapon == Grapple)
+            {
+                grappleable = true;
+                Player.GetComponent<GrapplingGun>().enabled = true;
+            }
+            else if (Grapple != null)
+            {
+                Player.GetComponent<GrapplingGun>().enabled = false;
+            }
+        }
+    }
+
+    private void HolsterWeapon(GameObject weapon, Transform holsterPosition)
+    {
+        if (weapon != null)
+        {
+            weapon.transform.SetParent(holsterPosition);
+            weapon.SetActive(false);
+            weapon.transform.localPosition = Vector3.zero;
+            weapon.transform.localRotation = Quaternion.identity;
+        }
     }
 
     public void RemoveWeapon(GameObject weapon)
     {
         Weapon weaponScript = weapon.GetComponent<Weapon>();
+
         switch (weaponScript.weapontype)
         {
             case Weapon.WeaponType.Primary:
-                if (Primary == weapon)
-                {
-                    Primary = null;
-                }
+                if (Primary == weapon) Primary = null;
                 break;
 
             case Weapon.WeaponType.Secondary:
-                if (Secondary == weapon)
-                {
-                    Secondary = null;
-                }
+                if (Secondary == weapon) Secondary = null;
                 break;
 
             case Weapon.WeaponType.Grenade:
-                if (Grenade == weapon)
-                {
-                    Grenade = null;
-                }
+                if (Grenade == weapon) Grenade = null;
                 break;
 
-            case Weapon.WeaponType.Grapple: // Added Grapple case
+            case Weapon.WeaponType.Grapple:
                 if (Grapple == weapon)
                 {
                     Grapple = null;
+                    grappleable = false;
+                    Player.GetComponent<GrapplingGun>().enabled = false;
                 }
                 break;
         }
 
-        if (HandSlot == weapon)
+        if (HandSlot == weapon) HandSlot = null;
+    }
+
+    private void Start()
+    {
+        InitialSetup();
+    }
+
+    private void InitialSetup()
+    {
+        // Disable the GrapplingGun script at the start of the game
+        if (Player.GetComponent<GrapplingGun>() != null)
         {
-            HandSlot = null;
+            Player.GetComponent<GrapplingGun>().enabled = false;
         }
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1) && Primary != null)
-        {
-            EquipWeapon(Primary);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && Secondary != null)
-        {
-            EquipWeapon(Secondary);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && Grenade != null)
-        {
-            EquipWeapon(Grenade);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && Grapple != null) // Added Grapple case
-        {
-            EquipWeapon(Grapple);
-        }
-    }
 }
+
+
